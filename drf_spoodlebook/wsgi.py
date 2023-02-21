b2bce03565dd88e5667efb5eeed95d4f967c173d
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-02-21 14:13:17 +0000

    set up drf_spoodlebook app for PP5

--- drf_spoodlebook/wsgi.py
+++ drf_spoodlebook/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for reactive project.
+WSGI config for drf_spoodlebook project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reactive.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_spoodlebook.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reactive.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_spoodlebook.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
